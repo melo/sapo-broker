ff93548b559dcc180eb1fac469b9cfffd72f93de
Luis Neves <luis.neves@co.sapo.pt>
2008-05-21 10:59:20 +0000

    add an index that helps speed thing up when there several queues stored but some of them have very low cardinality
    git-svn-id: svn://svn.softwarelivre.sapo.pt/broker/trunk@200 b5e76149-5b1e-0410-8289-f826a2598d57

--- gcs/base/src/pt/com/gcs/etc/create_schema.sql
+++ gcs/base/src/pt/com/gcs/etc/create_schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE IF NOT EXISTS Message (msg_id varchar(50), correlation_id varchar(50), destination varchar(255), priority int, mtimestamp bigint, expiration bigint, source_app varchar(100), content varchar, sequence_nr bigint, delivery_count int, local_only boolean DEFAULT false, PRIMARY KEY(msg_id, destination));
 CREATE TABLE IF NOT EXISTS VirtualQueue (queue_name varchar(255), PRIMARY KEY(queue_name));
+CREATE INDEX IF NOT EXISTS idx_dst_name ON Message(destination);
