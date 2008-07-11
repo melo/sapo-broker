837a5b7afddcfe3865f9c015dd9a71a4a2d73cd2
Luis Neves <luis.neves@co.sapo.pt>
2008-07-11 11:27:16 +0000

    add a couple of sample scripts that illustrate a common broker use case
    git-svn-id: svn://svn.softwarelivre.sapo.pt/broker/trunk@265 b5e76149-5b1e-0410-8289-f826a2598d57

---
+++ agent/sample_consumer.bat
@@ -0,0 +1,15 @@
+@echo off
+
+title Sample Consumer
+
+set CLASSPATH=./conf
+set CLASSPATH=%CLASSPATH%;./lib/*
+
+set JAVA_OPTS=%JAVA_OPTS% -Xverify:none -Xms16M -Xmx16M
+set JAVA_OPTS=%JAVA_OPTS% -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false -Dfile.encoding=UTF-8
+
+@echo on
+
+java %JAVA_OPTS% -cp "%CLASSPATH%" pt.com.broker.sample.Consumer -n /test/foo -d TOPIC
+
+pause
---
+++ agent/sample_producer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+java -server \
+-Xverify:none -Xms16M -Xmx16M \
+-Djava.awt.headless=true \
+-Djava.net.preferIPv4Stack=true \
+-Djava.net.preferIPv6Addresses=false \
+-Dfile.encoding=UTF-8 \
+-cp "./conf:./lib/*" \
+pt.com.broker.sample.Producer -n /test/foo -d TOPIC
