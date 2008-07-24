9394b2c0d3f06adcd427ccd875abf3811501c4f6
Luis Neves <luis.neves@co.sapo.pt>
2008-07-24 09:43:14 +0000

    add bits for compatibility with 1.5 JVM
    git-svn-id: svn://svn.softwarelivre.sapo.pt/broker/trunk@284 b5e76149-5b1e-0410-8289-f826a2598d57

---
+++ agent/jvm15/sample_consumer_15.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+cd ..
+
+classpath="./conf"
+
+for i in ./lib/*.jar
+do
+classpath=$classpath:$i
+done
+
+for i in ./jvm15/lib/*.jar
+do
+classpath=$classpath:$i
+done
+
+java -server \
+-Xverify:none -Xms16M -Xmx16M \
+-Djava.awt.headless=true \
+-Djava.net.preferIPv4Stack=true \
+-Djava.net.preferIPv6Addresses=false \
+-Dfile.encoding=UTF-8 \
+-cp $classpath \
+pt.com.broker.client.sample.Consumer -n /test/foo -d TOPIC
