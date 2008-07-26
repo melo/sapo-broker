8d24e2091c0e661d7e3e2e59ea50730ffb8c47cb
Luis Neves <luis.neves@co.sapo.pt>
2008-07-26 18:09:39 +0000

    patch by Pedro Melo <melo@simplicidade.org> that improve shell scripts to support Java 1.5
    git-svn-id: svn://svn.softwarelivre.sapo.pt/broker/trunk@293 b5e76149-5b1e-0410-8289-f826a2598d57

--- agent/broker.sh
+++ agent/broker.sh
@@ -2,6 +2,24 @@
 
 cd `dirname $0`
 
+# check version
+java -version 2>&1 | grep 1.5 > /dev/null
+if [ $? == 0 ] ; then # Yup, 1.5 still
+  echo Found Java version 1.5
+  classpath="./conf"
+
+  for i in ./jvm15/lib/*.jar; do
+    classpath=$classpath:$i
+  done
+
+  for i in ./lib/*.jar; do
+    classpath=$classpath:$i
+  done
+else # we assume 1.6 here
+  echo Found Java version 1.6
+  classpath="./conf:./lib/*"
+fi
+
 java -server \
 -Xverify:none -Xms256M -Xmx256M \
 -Djava.awt.headless=true \
@@ -9,5 +27,5 @@ java -server \
 -Djava.net.preferIPv6Addresses=false \
 -Dfile.encoding=UTF-8 \
 -Dconfig-path=./conf/main_example.xml \
--cp "./conf:./lib/*" \
+-cp $classpath \
 pt.com.broker.Start
--- agent/sample_producer.sh
+++ agent/sample_producer.sh
@@ -2,11 +2,29 @@
 
 cd `dirname $0`
 
+# check version
+java -version 2>&1 | grep 1.5 > /dev/null
+if [ $? == 0 ] ; then # Yup, 1.5 still
+  echo Found Java version 1.5
+  classpath="./conf"
+
+  for i in ./jvm15/lib/*.jar; do
+    classpath=$classpath:$i
+  done
+
+  for i in ./lib/*.jar; do
+    classpath=$classpath:$i
+  done
+else # we assume 1.6 here
+  echo Found Java version 1.6
+  classpath="./conf:./lib/*"
+fi
+
 java -server \
 -Xverify:none -Xms16M -Xmx16M \
 -Djava.awt.headless=true \
 -Djava.net.preferIPv4Stack=true \
 -Djava.net.preferIPv6Addresses=false \
 -Dfile.encoding=UTF-8 \
--cp "./conf:./lib/*" \
+-cp $classpath \
 pt.com.broker.client.sample.Producer -n /test/foo -d TOPIC
