b93c506fc68a95ee13877dc274d816ebac09b141
Luis Neves <luis.neves@co.sapo.pt>
2008-07-28 09:12:14 +0000

    start publishing the queue size in the "/system/stats/queue-size/#QUEUE_NAME#"
    git-svn-id: svn://svn.softwarelivre.sapo.pt/broker/trunk@298 b5e76149-5b1e-0410-8289-f826a2598d57

--- gcs/base/src/pt/com/gcs/messaging/QueueCounter.java
+++ gcs/base/src/pt/com/gcs/messaging/QueueCounter.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import pt.com.gcs.conf.GcsInfo;
+
 class QueueCounter implements Runnable
 {
 	private static Logger log = LoggerFactory.getLogger(QueueCounter.class);
@@ -26,6 +28,22 @@ class QueueCounter implements Runnable
 			else if ((cnt == 0) && !qp.emptyQueueInfoDisplay.getAndSet(true))
 			{
 				log.info("Queue '{}' is empty.", qp.getDestinationName());
+			}
+
+			try
+			{
+				Message cnt_message = new Message();
+				String dName = String.format("/system/stats/queue-size/#%s#", qp.getDestinationName());
+				String content = GcsInfo.getAgentName() + "#" + qp.getDestinationName() + "#" + cnt;
+				cnt_message.setDestination(dName);
+				cnt_message.setContent(content);
+
+				Gcs.publish(cnt_message);
+			}
+			catch (Throwable error)
+			{
+				String emsg = String.format("Could not publish queue counter for '{}'", qp.getDestinationName());
+				log.error(emsg, error);
 			}
 		}
 	}
