67c2ee8251eddc197ce77ffdc9251d72a9a428dc
J. Cameijo Cerdeira <cerdeira@co.sapo.pt>
2008-07-31 14:55:57 +0000

    client now acknowledges messages subscribed in topic_as_queue mode; improved error handling; added publish function
    git-svn-id: svn://svn.softwarelivre.sapo.pt/broker/trunk@314 b5e76149-5b1e-0410-8289-f826a2598d57

--- clients/erlang-Component-BrokerTCP/bc.erl
+++ clients/erlang-Component-BrokerTCP/bc.erl
@@ -6,7 +6,7 @@
 -import(xmerl_scan).
 
 
--export([init/1,subscribe/2,receive_msg/1,receive_loop/2,go/0]).
+-export([init/1,subscribe/2,publish/3,receive_msg/1,receive_loop/2,go/0]).
 
 
 -include("xmerl.hrl").
@@ -25,7 +25,7 @@ loop() ->
 
 go() ->
 	%{ok,F} =init([{msg_type,'TOPIC_AS_QUEUE'},{host,"ejabberd1.m3.bk.sapo.pt"}]),
-	{ok,F} =init([{msg_type,'TOPIC'},{host,"10.135.5.110"}]),
+	{ok,F} =init([{msg_type,'TOPIC_AS_QUEUE'},{host,"10.135.5.110"}]),
 	subscribe(F,["/sapo/messenger/raw/sessions", "/sapo/messenger/raw/presences"]),
 	%X= receive_msg(F),
 	%io:format("go: Result (~p)~n",[X])
@@ -64,37 +64,59 @@ init(Conf) when is_list(Conf) ->
 .		
 
 
-subscribe({Socket, [MType|Dest], Timeout}, [H|T]) -> 
-	case MType of 
-		'TOPIC_AS_QUEUE' -> RDest = Dest ++ "@" ++ H;
-		_ -> RDest = H
+publish({Socket, [MT|Name], _Timeout}, Topic, Data) -> 
+	case MT of 
+		'TOPIC_AS_QUEUE' -> Dest = Name ++ "@" ++ Topic;
+		_ -> Dest = Topic
+	end,
+
+ Msg = 
+  "<soapenv:Envelope xmlns:soapenv='http://www.w3.org/2003/05/soap-envelope'>
+	<soapenv:Body><Publish xmlns='http://services.sapo.pt/broker'><BrokerMessage>
+  <DestinationName>" ++ Dest ++ "</DestinationName>
+	<TextPayload>" ++ Data ++ "/TextPayload></BrokerMessage></Publish>
+  </soapenv:Body></soapenv:Envelope>",
+
+	gen_tcp:send(Socket,Msg)
+.
+
+
+subscribe({Socket, [MT|Name], Timeout}, [H|T]) -> 
+	case MT of 
+		'TOPIC_AS_QUEUE' -> Dest = Name ++ "@" ++ H;
+		_ -> Dest = H
 	end,
 	
-	Msg = "<soapenv:Envelope xmlns:soapenv='http://www.w3.org/2003/05/soap-envelope'><soapenv:Body>
-<Notify xmlns='http://services.sapo.pt/broker'><DestinationName>" ++ 
-RDest ++ "</DestinationName><DestinationType>" ++ 
-atom_to_list(MType) ++ "</DestinationType></Notify></soapenv:Body></soapenv:Envelope>",
+	Msg = 
+		"<soapenv:Envelope xmlns:soapenv='http://www.w3.org/2003/05/soap-envelope'>
+		<soapenv:Body><Notify xmlns='http://services.sapo.pt/broker'>
+		<DestinationName>" ++ Dest ++ "</DestinationName>
+		<DestinationType>" ++ atom_to_list(MT) ++ "</DestinationType>
+		</Notify></soapenv:Body></soapenv:Envelope>",
     
 	case gen_tcp:send(Socket,Msg) of
-		ok -> subscribe({Socket, [MType|Dest], Timeout}, T);
+		ok -> subscribe({Socket, [MT|Name], Timeout}, T);
 		What -> 
 			io:format("subscribe: send failed ~p~n",[What]),
 			What
 	end;
 
-subscribe(_, []) -> [].
+subscribe(_, []) -> ok.
 
 
-receive_msg({Socket,_, Timeout}) -> 
+receive_msg({Socket, X, Timeout}) -> 
 	%io:format("recv: timeout ~p~n",[Timeout]),
 
 	case gen_tcp:recv(Socket,0,Timeout) of
-		{ok, Data} -> 
+		{ok, Data} ->
 			{XML, _} = xmerl_scan:string(binary_to_list(Data)),
-			find_elements(
-				['TextPayload', 'MessageId', 'DestinationName' ],XML#xmlElement.content);
+			RV = find_elements(
+				['TextPayload', 'MessageId', 'DestinationName' ],XML#xmlElement.content),
 
-		{error, closed } -> []
+			acknowledge(Socket, X, RV),
+			RV;
+
+		{error, closed} -> []
 	end
 .
 
@@ -103,20 +125,47 @@ receive_loop(X,Pid) when is_pid(Pid) ->
 	receive_loop(X,fun(Data) -> Pid ! {self(), Data} end)
 ;
 
-receive_loop({Socket,X, Timeout}, F) -> 
+receive_loop({Socket,X, _Timeout}, F) -> 
 	%io:format("recv: timeout ~p~n",[Timeout]),
-	case gen_tcp:recv(Socket,0,Timeout) of
+	case gen_tcp:recv(Socket,0) of
 		{ok, Data} -> 
 			{XML, _} = xmerl_scan:string(binary_to_list(Data)),
-			F(find_elements(
-				['TextPayload', 'MessageId', 'DestinationName'], XML#xmlElement.content)),
-		
-			receive_loop({Socket,X, Timeout},F);
+			
+			RV = find_elements(
+				['TextPayload', 'MessageId', 'DestinationName'], XML#xmlElement.content),
+
+			try F(RV)
+			catch
+				throw:X -> io:format("receive_loop: function raised exception ~p~n",[X])
+			end,
+
+			acknowledge(Socket, X, RV),
+			receive_loop({Socket,X, _Timeout},F);
 			
 		{error, closed} -> [];
 		
 		{error, _} ->
-			receive_loop({Socket,X, Timeout},F)
+			receive_loop({Socket,X, _Timeout},F)
+	end
+.
+
+
+acknowledge(Socket, X, Data) ->
+	case X of 
+		['TOPIC_AS_QUEUE', _] ->
+			{value, {_, Id}} = lists:keysearch('MessageId',1,Data),
+			{value, {_, Dest}} = lists:keysearch('DestinationName',1,Data),
+
+			Msg = 
+				"<soapenv:Envelope xmlns:soapenv='http://www.w3.org/2003/05/soap-envelope'>
+				<soapenv:Body><Acknowledge xmlns='http://services.sapo.pt/broker'>
+				<DestinationName>" ++ Dest ++ "</DestinationName>
+				<MessageId>" ++ Id ++ "</MessageId></Acknowledge>
+				</soapenv:Body></soapenv:Envelope>",
+
+			gen_tcp:send(Socket,Msg);
+
+		_ -> ok
 	end
 .
 
@@ -128,18 +177,18 @@ msg_type(Conf) ->
 				true -> 
 					case V of
 						'TOPIC_AS_QUEUE' ->
-							case lists:keysearch(hostname,1,Conf) of
-								{value, {_, Hostname}} -> Hostname;
-								_ -> {_, Hostname} = inet:gethostname()
+							case lists:keysearch(name,1,Conf) of
+								{value, {_, Name}} -> Name;
+								_ -> {_, Name} = inet:gethostname()
 							end,
-							{ok, [V, Hostname]};
+							{ok, [V, Name]};
 						_ -> {ok, [V] }
 					end;
 					
 				false -> {error, einval}
 			end;
 			
-		_ -> {ok, 'TOPIC'}
+		_ -> {ok, ['TOPIC']}
 	end
 .
 
